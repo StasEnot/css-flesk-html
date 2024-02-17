97533fbbfe8e0983543895200d79d9b5ee475b40
StasEnot <115869874+StasEnot@users.noreply.github.com>
2024-02-17 11:39:12 +0200

    22
    22 22 22 22

--- pr21/main.py
+++ pr21/main.py
@@ -6,13 +6,29 @@ app = Flask(__name__)
 def Index():
     return render_template('index.html')
 
+@app.route('/<name>')
+def user_index(name):
+    return  render_template( 'index.html', user_name=name)
+
 @app.route('/Article1')
 def Article():
     return '<h1>Незабаром ви побачите наші статті Stas</h1>'
 
 @app.route('/Admin1')
 def Admin():
-    return '<h1>Це адміни<h1><br><p>напишіть наші помилкиStas</p><br> '
+    return ('<h1>Це адміни<h1><br><p>напишіть наші помилки Stas</p><br> ')
+   # return ('<textarea name="text" required rows="15" cols="100" placeholder="Your text"></textarea>')
+
+
+@app.route('/Login')
+def Login():
+    return render_template('logo.html')
+
+@app.route('/Add_Article')
+def Add_Article():
+    return render_template('add_article.html')
+
+
 
 if __name__ =="__main__":
     app.run()
